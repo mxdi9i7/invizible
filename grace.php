30bcbb255dba4846932c8bbfe136adf0cbe60cd5
mxdi9i7 <peter.zheng88228@gmail.com>
2018-11-06 20:33:05 -0500

    updaated routing and xiaomi

---
+++ grace.php
@@ -0,0 +1,59 @@
+<?php include './partials/header.html' ?>
+<?php include './partials/nav.html' ?>
+
+
+<!-- 1 -->
+<div class="caseSection">
+    <div class="caseOverlay">
+    </div>
+
+    <div class="caseBanner" style="background-image:url('../banner/grace.jpg')"> 
+        <div class="caseBannerContainer">
+            <div class="casePortrait">
+                <img src="../imgCase/thumbnail-grace2.png" alt="">
+            </div>
+    </div>
+
+</div>
+
+<!-- 2 -->
+<div class="caseCarousel">
+    <div class="caseCarouselContent">
+
+        <!-- left part -->
+        <div class="caseCaroText">
+            <h6>
+            Grace Chen Runway Show at China Fashion Gala 2018
+            <br>
+            #Show Production #Model Casting
+            <br>
+            <br>
+            As an outstanding couture designer in China, Grace Chen was invited by China Fashion Gala to present a full-length couture fashion show as the sole highlight designer of the year. InVizible was enlisted to plan and coordinate the show, including model casting, garments coordination, hair & make-up communication, show directing, etc. With a limited budget, we’ve hired exceedingly peerless models to make the show as a milestone to fashion diplomacy for Grace Chen.
+            </h6>
+        </div>
+        <div class="caseCarouselSection">
+            <div class="owl-carousel owl-theme case-carousel">
+                <div class="item">
+                    <img class="d-block w-100" src="../imgCase/grace1.png" alt="Second slide">
+                </div>
+                <div class="item">
+                    <img class="d-block w-100" src="../imgCase/grace2.png" alt="Second slide">
+                </div>
+                <div class="item">
+                    <img class="d-block w-100" src="../imgCase/grace3.png" alt="Second slide">
+                </div>
+                <div class="item">
+                    <img class="d-block w-100" src="../imgCase/grace4.png" alt="Second slide">
+                </div>
+                <div class="item">
+                    <img class="d-block w-100" src="../imgCase/grace5.png" alt="Second slide">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include './partials/seemore.html' ?>
+
+
+<?php include './partials/footer.html' ?>
