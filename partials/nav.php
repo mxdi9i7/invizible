daceb1eea4fdd1c2b54c70066a19eb1d81776294
mxdi9i7 <peter.zheng88228@gmail.com>
2019-03-01 22:53:30 -0500

    simple fast push

---
+++ partials/nav.php
@@ -0,0 +1,16 @@
+
+<nav>
+    <div class="navContainer">
+        <div class="navLogo">
+            <a href="index.html"><img src="img/logo-white-words.png" alt=""></a>
+        </div>
+        <div class="links">
+            <ul>
+                <li><a href="index.html#about">ABOUT</a></li>
+                <li><a href="index.html#service">OUR SERVICES</a></li>
+                <li><a href="work.html">WORK</a></li>
+                <li><a href="contact.html">CONTACT</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
